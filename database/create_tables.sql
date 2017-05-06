a048dbf56d7d75e76f3307b9bfba62736deca887
aleksanchezz <aleksanchezz@gmail.com>
2017-05-06 16:57:01 +0300

    Add database table creation script

---
+++ database/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id          integer NOT NULL PRIMARY KEY,               -- PK
+    name        text NULL,
+    code_name   text NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS books
+(
+    id          integer NOT NULL PRIMARY KEY,               -- PK
+    title       text NULL,
+    author_id   integer NOT NULL REFERENCES authors (id),   -- FK
+    genre       text NUll,
+    date        text NULL,
+    code_name   text NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS texts
+(
+    id              integer NOT NULL PRIMARY KEY,            -- PK
+    book_id         integer NOT NULL REFERENCES books (id),  -- FK
+    name            text NULL,
+    values_array    integer[],
+    parts_array     integer[],
+    code_name       text NULL UNIQUE
+);
