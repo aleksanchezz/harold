f817f0255b354f19bb03550b0c0879e97f33889d
aleksanchezz <aleksanchezz@gmail.com>
2017-05-06 16:09:15 +0300

    Add fiction book file parser

---
+++ python/main.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+"""
+Точка входа в систему анализа текстов "Harold"
+"""
+from parser.fiction_book import FictionBook
+
+
+def main():
+
+    fb = FictionBook('/home/aln/diplom/example.fb2')
+
+    return 0
+
+if __name__ == "__main__":
+    main()
