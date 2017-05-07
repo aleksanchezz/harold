07e90da3d96a7999d6b4ef03888e178590849239
aleksanchezz <aleksanchezz@gmail.com>
2017-05-07 12:19:45 +0300

    Add dummy database processing
    	* simple testing fixtures
    	* settings file
    	* simple psycopg2 fot now

---
+++ python/config/settings.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import sys
+
+DATABASE_HOST = '127.0.0.1'
+
+# DB Postgres
+PHAROLD_SETTINGS = {
+    'host': DATABASE_HOST,
+    'port': 5432,
+    'user': 'harold',
+    'dbname': 'harold'
+}
